e7ff5ccc9ddfbba8b26818c19a78e3718e729fad
Fernando Lopez <fernandojalope343@gmail.com>
2021-03-28 19:15:27 -0400

    movies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import 'bulma/css/bulma.css'
+import { Title } from './components/Title';
+import { SearchForm } from './components/SearchForm';
+import { MoviesList } from './components/MoviesList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = { results: [] }
+
+  _handleResults = results => {
+    this.setState({ results })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Title>Search Movies</Title>
+        <div className="SearchForm-wrapper">
+          <SearchForm onResults={this._handleResults} />
+        </div>
+        {this.state.results.length === 0
+          ? <p>Sin resultados</p>
+          : <MoviesList movies={this.state.results} />
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
